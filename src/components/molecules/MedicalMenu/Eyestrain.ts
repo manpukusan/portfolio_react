205b9358a3b765481aad474716e38655828eec5e
EOS-S01\saito.t <t.saito@eos-firm.co.jp>
2022-07-01 16:14:59 +0900

    ポケット食堂のメニュー作成

--- src/components/molecules/MedicalMenu/Eyestrain.ts
+++ src/components/molecules/MedicalMenu/Eyestrain.ts
@@ -1,20 +1,23 @@
 export const eyestrain = [
   {
     id: 1,
-    title: 'ブルーベリー定食',
-    effect: '貧血　→　鉄分　イコール　レバー',
+    title: '★ひとみうるおう定食★',
+    effect:
+      'カラカラな眼でがんばってPCを見つめているあなた。瞳にうるおいをあげましょう。レバーは、鉄分だけでなく粘膜を保護してくれるビタミンAもたっぷりなのでうるおい補給してあげましょう。',
     imagePath: './public/images/eyestrain/eyestrain001.png',
   },
   {
     id: 1,
-    title: 'ルテイン定食',
-    effect: '貧血　→　鉄分　イコール　レバー',
+    title: '★抗酸化力爆発アスタキサンチン丼★',
+    effect:
+      '酸化しない、させないためには、赤のパワーアスタキサンチン先生の力をかりましょう！鮭といくらの親子丼はいかが？ビタミンAの多い海苔もご一緒に。疲れ目、老眼にも効果的な抗酸化パワー♪',
     imagePath: './public/images/eyestrain/eyestrain002.png',
   },
   {
     id: 1,
-    title: '眼精疲労定食',
-    effect: '貧血　→　鉄分　イコール　レバー',
+    title: '★薬膳ハオチー餃子★',
+    effect:
+      'ただの餃子ではありません。疲れ目、視力低下には、食べる目薬と言われるクコの実とビタミンたっぷりの菊花で回復させましょう。楊貴妃が毎日食べたと言われるクコの実には、抗酸化力も強くアンチエイジングにもおすすめ。',
     imagePath: './public/images/eyestrain/eyestrain003.png',
   },
 ];
--- src/components/organisms/Pocket/PocketHeader.tsx
+++ src/components/organisms/Pocket/PocketHeader.tsx
@@ -3,7 +3,10 @@ import { FC, memo } from 'react';
 
 export const PocketHeader: FC = memo(() => {
   return (
-    <VStack fontSize={{ base: 'xl', md: '2xl', lg: '3xl', xl: '4xl' }}>
+    <VStack
+      fontSize={{ base: 'xl', md: '2xl', lg: '3xl', xl: '4xl' }}
+      mb={{ base: 4, md: 8, lg: 12, xl: 16 }}
+    >
       <Heading as="h1" textAlign="center" mt={8}>
         ポケット食堂
       </Heading>
